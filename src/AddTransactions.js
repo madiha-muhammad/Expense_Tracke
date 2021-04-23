636b58cd3e85a32caac74fb6be2f5a57da1e287d
madiha <78408471+madiha-muhammad@users.noreply.github.com>
2021-04-23 14:49:38 +0500

    Seprate UI in new js file and rearrange CSS

---
+++ src/AddTransactions.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Transaction(){
+    let transaction_list = [
+        {amount: 500, desc: 'Cash'},
+        {amount: -40, desc: 'Book'},
+        {amount: -200, desc: 'Camera'}
+    ]
+
+    return (
+<div></div>
+    )
+}
+
+export default Transaction;
+
